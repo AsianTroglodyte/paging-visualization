aea1605e56779008d0d3d45d6b6aec8ea5446b0e
Karan Swansi <kswansi2002@gmail.com>
2026-02-27 07:28:19 -0500

    changed MMU arrows and formatting

--- src/components/mmu-card.tsx
+++ src/components/mmu-card.tsx
@@ -30,7 +30,7 @@ export function MmuCard({mmu, className}: {mmu: MmuState, className: string}) {
                 </CardDescription>
             </CardHeader>
             <CardContent className="">
-                <div className="grid grid-cols-[2.5rem_2.5rem_2.5rem_2.5rem_2.5rem_2.5rem_2.5rem_2.5rem] grid-rows-[auto_1.5rem_3rem_3rem_3rem_1.5rem] items-start">
+                <div className="grid grid-cols-[2.5rem_2rem_2rem_2rem_2rem_1rem_2rem_2rem_2rem] grid-rows-[auto_1.5rem_3rem_3rem_3rem_1.5rem] items-start">
                     <div className="flex flex-col h-full justify-between items-center h-7 col-span-2 row-span-7">
                         <p className="text-sm text-muted-foreground 
                         font-medium flex items-center pr-2 justify-end pt-5">
@@ -42,6 +42,9 @@ export function MmuCard({mmu, className}: {mmu: MmuState, className: string}) {
                         </p>
                     </div>
                     <div className="text-base flex justify-center font-semibold h-7 col-span-3">VPN</div>
+
+                    <div className="text-base flex justify-center font-semibold h-7 row-span-8"></div>
+
                     <div className="text-base flex justify-center font-semibold h-7 col-span-3">offset</div>
 
                     {vpnArray.map((bit, index) => {
@@ -54,6 +57,7 @@ export function MmuCard({mmu, className}: {mmu: MmuState, className: string}) {
                             </p>)
                     })}
 
+
                     {offsetString.split("").map((bit, index) => {
                         return (<p key={index} className="text-base flex justify-center items-center font-semibold border">{bit}</p>)
                     })}
@@ -65,31 +69,30 @@ export function MmuCard({mmu, className}: {mmu: MmuState, className: string}) {
                             bit === "" 
                             ? "invisible" 
                             :"flex justify-center items-center"}>
-                            <svg viewBox="0 0 24 72" className="text-foreground" fill="currentColor">
-                                <line x1="12" y1="4" x2="12" y2="17" stroke="currentColor" 
-                                strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                                <path d="M12 23 L9 16 L15 16Z" />
+                            <svg viewBox="0 0 24 30" className="text-foreground" fill="currentColor">
+                                <line x1="12" y1="4" x2="12" y2="25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 28 L9 21 L15 21Z" />
                             </svg>
                         </div>)
                     })}
                     
                     {/* Offset Arrow Divs */}
                     <div className="flex justify-center items-center row-span-3">
-                        <svg viewBox="0 0 24 84" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 81 L9 74 L15 74 Z" />
+                        <svg viewBox="0 0 24 100" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="95" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 100 L9 93 L15 93 Z" />
                         </svg>
                     </div>
                     <div className="flex justify-center items-center row-span-3">
-                        <svg viewBox="0 0 24 84" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 81 L9 74 L15 74 Z" />
+                        <svg viewBox="0 0 24 100" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="95" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 100 L9 93 L15 93 Z" />
                         </svg>
                     </div>                    
                     <div className="flex justify-center items-center row-span-3">
-                        <svg viewBox="0 0 24 84" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="75" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 81 L9 74 L15 74 Z" />
+                        <svg viewBox="0 0 24 100" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="95" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 100 L9 93 L15 93 Z" />
                         </svg>
                     </div>
 
@@ -99,21 +102,21 @@ export function MmuCard({mmu, className}: {mmu: MmuState, className: string}) {
 
 
                     <div className="flex justify-center items-center ">
-                        <svg viewBox="0 0 24 72" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 23 L9 16 L15 16Z" />
+                        <svg viewBox="0 0 24 30" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 28 L9 21 L15 21Z" />
                         </svg>
                     </div>
                     <div className="flex justify-center items-center ">
-                        <svg viewBox="0 0 24 72" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 23 L9 16 L15 16Z" />
+                        <svg viewBox="0 0 24 30" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                            <path d="M12 28 L9 21 L15 21Z" />
                         </svg>
                     </div>
                     <div className="flex justify-center items-center ">
-                        <svg viewBox="0 0 24 72" className="text-foreground" fill="currentColor">
-                            <line x1="12" y1="4" x2="12" y2="17" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
-                            <path d="M12 23 L9 16 L15 16Z" />
+                        <svg viewBox="0 0 24 30" className="text-foreground" fill="currentColor">
+                            <line x1="12" y1="4" x2="12" y2="25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" fill="none" />
+                        <path d="M12 28 L9 21 L15 21Z" />
                         </svg>
                     </div>
 
